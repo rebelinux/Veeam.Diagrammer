1ebe3999eba2261b20ba589fd995ed7d2f6d84bb
Jonathan Colon <rebelinux@gmail.com>
2024-01-03 01:00:54 -0400

    Improved Debug feature

--- Src/Private/Get-HtmlLabel.ps1
+++ Src/Private/Get-HtmlLabel.ps1
@@ -17,6 +17,6 @@ Function Get-HTMLLabel {
     if ($ICON -ne 'NoIcon') {
         return "<TABLE border='0' cellborder='0' cellspacing='20' cellpadding='10'><TR><TD ALIGN='center' colspan='1'><img src='$($ICON)'/></TD></TR><TR><TD ALIGN='center'>$Label</TD></TR></TABLE>"
     } else {
-        return "<TABLE border='0' cellborder='1' cellspacing='20' cellpadding='10' ><TR><TD bgcolor='#FFCCCC' ALIGN='center' colspan='1'>Veeam Logo</TD></TR><TR><TD bgcolor='#FFCCCC' ALIGN='center'>$Label</TD></TR></TABLE>"
+        return "<TABLE border='0' cellborder='0' cellspacing='20' cellpadding='10' ><TR><TD bgcolor='#FFCCCC' ALIGN='center' colspan='1'>Veeam Logo</TD></TR><TR><TD bgcolor='#FFCCCC' ALIGN='center'>$Label</TD></TR><TR><TD ALIGN='center'><font color='red'>Debug ON</font></TD></TR></TABLE>"
     }
 }
