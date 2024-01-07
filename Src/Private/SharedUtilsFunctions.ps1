ce54127acadf915f9a8478a13658f608c97ec746
Jonathan Colon <rebelinux@gmail.com>
2024-01-07 15:47:00 -0400

    Fix PSScriptAnalyzer warning

--- Src/Private/SharedUtilsFunctions.ps1
+++ Src/Private/SharedUtilsFunctions.ps1
@@ -2,20 +2,26 @@
 function Remove-SpecialChar {
     <#
     .SYNOPSIS
-        Used by Veeam.Diagrammer to remove unsupported characters.
+        Used by Veeam.Diagrammer to remove unsupported graphviz dot characters.
     .DESCRIPTION
     .NOTES
         Version:        0.1.0
         Author:         Prateek Singh
     .EXAMPLE
+        Remove-SpecialChar -String "Non Supported chars ()[]{}&."
     .LINK
     #>
+
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'Low')]
     param(
         [string]$String,
         [string]$SpecialChars = "()[]{}&."
     )
 
-    $String -replace $($SpecialChars.ToCharArray().ForEach( { [regex]::Escape($_) }) -join "|"), ""
+    if ($PSCmdlet.ShouldProcess($String, ("Remove {0} chars" -f $SpecialChars,$String)))
+    {
+        $String -replace $($SpecialChars.ToCharArray().ForEach( { [regex]::Escape($_) }) -join "|"), ""
+    }
 }
 
 
